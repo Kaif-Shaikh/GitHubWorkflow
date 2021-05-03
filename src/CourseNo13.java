20a72568d0304a87c6ea81eb2d88d4371b6ddd2c
DESKTOP-I4TAMJQ\Shaikh Kaif <kaifshaikh1999@gmail.com>
2021-05-03 11:46:57 +0530

    First commit...

---
+++ src/CourseNo13.java
@@ -0,0 +1,13 @@
+class CourseNo13 {
+	
+	public static void main(String[]args) {
+		
+		String hungry = "yes";
+		
+		if (hungry == "yes")
+			System.out.println("Eating something....");
+		else {
+			System.out.println("Do Work....");
+		}
+	}
+}
